0721e24f9f4ffb43fbb5727f981601fcdcf8d1b6
VFDan <44413950+VFDan@users.noreply.github.com>
2021-03-25 18:29:58 -0400

    Needs to be 38 not 36

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -4,6 +4,6 @@ setlocal enabledelayedexpansion
 SET "FPARG=%1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
-SET "FPARG=%FPARG:~0,36%"
+SET "FPARG=%FPARG:~0,38%"
 echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"|set /p "FPARG=")
 start "" "CLIFp.exe" --quiet --auto %FPARG%
