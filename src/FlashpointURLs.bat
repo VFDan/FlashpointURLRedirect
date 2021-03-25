28c2fe38212a23e50e1216492601ab332317716e
arcturus108 <76753099+arcturus108@users.noreply.github.com>
2021-03-25 19:24:19 -0300

    Add regex to validate UUID
    Add regex to validate UUID and injection protection

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -5,4 +5,5 @@ SET "FPARG=%1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
 SET "FPARG=%FPARG:~0,36%"
-echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"
+echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"|set /p "FPARG="
+start "" "CLIFp.exe" --quiet --auto !FPARG!
