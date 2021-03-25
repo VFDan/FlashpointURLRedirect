4cd98bf0080c522a6512f919cbf713dffb29251e
VFDan <44413950+VFDan@users.noreply.github.com>
2021-03-25 18:52:50 -0400

    Better practices
    UUIDs are 36 chars long, so the filter should be 36, not 38.

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -1,9 +1,9 @@
 @echo off
 cd /D "%~dp0"
 setlocal enabledelayedexpansion
-SET "FPARG=%1"
+SET "FPARG=%~1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
-SET "FPARG=%FPARG:~0,38%"
+SET "FPARG=%FPARG:~0,36%"
 echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"|set /p "FPARG=")
 start "" "CLIFp.exe" --quiet --auto !FPARG!
