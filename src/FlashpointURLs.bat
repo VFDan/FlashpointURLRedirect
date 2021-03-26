86e9e627e6c95739c859cd9b2ebbce646fe86d9e
arcturus108 <76753099+arcturus108@users.noreply.github.com>
2021-03-26 01:49:41 -0300

    Fix set /p error handling
    Stop set /p from re-assigning FPARG's original value when findstr fails + error message

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -5,5 +5,6 @@ SET "FPARG=%~1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
 SET "FPARG=%FPARG:~0,36%"
-echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"|set /p "FPARG=")
+echo(!!FPARG!!|findstr /r /i /x "[0-9a-f-]*"|set /p "FPARG="
+If !ERRORLEVEL! NEQ 0 echo Error, "GUID is invalid" & exit /b
 start "" "CLIFp.exe" --quiet --auto !FPARG!
