c3e1c54ca36c92343c5fe474d419619269107f6e
VFDan <44413950+VFDan@users.noreply.github.com>
2021-03-26 13:51:06 -0400

    Create dialog box on bad UUID

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -5,6 +5,13 @@ SET "FPARG=%~1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
 SET "FPARG=%FPARG:~0,36%"
+
 echo(!!FPARG!!|findstr /r /i /x "[0-9a-f-]*"|set /p "FPARG="
-If !ERRORLEVEL! NEQ 0 echo Error, "GUID is invalid" & exit /b
-start "" "CLIFp.exe" --quiet --auto !FPARG!
+If !ERRORLEVEL! NEQ 0 (
+	ECHO MsgBox "GUID !FPARG! is invalid.",0,"FlashpointURLRedirect">%TMP%\TMP.VBS
+	WSCRIPT /nologo %TMP%\TMP.VBS
+	DEL %TMP%\TMP.VBS
+	exit /b
+) Else (
+	start "" "CLIFp.exe" --quiet --auto !FPARG!
+)
