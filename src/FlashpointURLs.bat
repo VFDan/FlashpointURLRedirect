b09bfa50aeece5fbdad2101f91b105f3af2ba655
VFDan <44413950+VFDan@users.noreply.github.com>
2021-03-25 20:27:53 -0400

    Stricter Regex

--- src/FlashpointURLs.bat
+++ src/FlashpointURLs.bat
@@ -5,5 +5,5 @@ SET "FPARG=%~1"
 SET "FPARG=%FPARG:/=%"
 SET "FPARG=%FPARG:flashpoint:=%"
 SET "FPARG=%FPARG:~0,36%"
-echo(!!FPARG!!|findstr /r /x "[0-9a-f-]*"|set /p "FPARG=")
+echo(!!FPARG!!|findstr /r /x "^[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}$"|set /p "FPARG=")
 start "" "CLIFp.exe" --quiet --auto !FPARG!
